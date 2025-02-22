1fc8e829158b7b58f054fd4e7750ed7349cf6215
Attila Oláh <attila@dorn.haus>
2025-02-22 14:20:21 +0100

    Give Cilium more time to complete upgrading. (#157)

--- manifests/kube-system/cilium/ks.yaml.nix
+++ manifests/kube-system/cilium/ks.yaml.nix
@@ -1,6 +1,9 @@
 {k, ...}:
 k.fluxcd.kustomization ./. rec {
-  app.spec.prune = false;
+  app.spec = {
+    prune = false;
+    timeout = "30m";
+  };
   config.spec = {
     inherit (app.spec) prune;
     dependsOn = map k.fluxcd.dep [./app];
