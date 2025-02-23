95af6b960ff61c085e1efef1ca9613cd50fa7c0e
Attila Oláh <attila@dorn.haus>
2025-02-23 14:17:03 +0100

    Switch to using the operator.

--- manifests/observability/jaeger-operator/app/values.yaml.nix
+++ manifests/observability/jaeger-operator/app/values.yaml.nix
@@ -1,31 +1,18 @@
 # https://artifacthub.io/packages/helm/jaegertracing/jaeger-operator#configuration
 {
-  # Simple all-in-one configuration.
-  # allInOne.enabled = true;
-  # agent.enabled = false;
-  # collector.enabled = false;
-  # query.enabled = false;
-  # provisionDataStore.cassandra = false;
-  # storage.type = "memory";
-  # jaeger:
-  #   # Create a Jaeger instance.
-  #   create: true
-  #   spec:
-  #     strategy: allInOne
-  #     storage:
-  #       type: memory
-  #       options:
-  #         memory:
-  #           max-traces: 1000
-  #     ingress:
-  #       enabled: true
-  #       annotations:
-  #         kubernetes.io/ingress.class: nginx
-  #       hosts:
-  #       - jaeger.net-kub-test-1.nmag.ch
-  #       security: none
-  #
-  #
+  jaeger = {
+    # Create a Jaeger instance.
+    create = true;
+    spec = {
+      strategy = "allInOne";
+      storage = {
+        type = "memory";
+        options.memory.max-traces = 1000;
+      };
+      ingress.enabled = false;
+    };
+  };
+
   # Ue a ClusterRole to allow listing ingress classes.
   rbac.clusterRole = true;
 }
