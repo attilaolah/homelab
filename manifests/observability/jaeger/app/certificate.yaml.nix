c2d73a329f1e7de8e4c650f50274ae7bd1a1e5e6
Attila Oláh <attila@dorn.haus>
2025-03-10 08:42:20 +0000

    Add jaeger collector certificate.

--- manifests/observability/jaeger/app/certificate.yaml.nix
+++ manifests/observability/jaeger/app/certificate.yaml.nix
@@ -1,15 +1,23 @@
-{k, ...}:
-k.api "Certificate.cert-manager.io" (let
-  name = "jaeger-query";
-in {
-  metadata = {inherit name;};
-  spec = {
-    secretName = "${name}-tls";
-    issuerRef = {
-      kind = "ClusterIssuer";
-      name = "internal-ca";
-    };
-    commonName = name;
-    dnsNames = [name];
-  };
-})
+{k, ...}: let
+  namespace = k.nsname ./.;
+in
+  map (
+    component:
+      k.api "Certificate.cert-manager.io" (let
+        name = "jaeger-${component}";
+      in {
+        metadata = {inherit name;};
+        spec = {
+          secretName = "${name}-tls";
+          issuerRef = {
+            kind = "ClusterIssuer";
+            name = "internal-ca";
+          };
+          commonName = name;
+          dnsNames = [
+            "${name}.${namespace}.svc"
+            name
+          ];
+        };
+      })
+  ) ["query" "collector"]
