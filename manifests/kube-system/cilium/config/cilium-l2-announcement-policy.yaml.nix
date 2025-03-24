6899d7dbda1446e98089917005968c3a36152871
Attila Oláh <attila@dorn.haus>
2025-03-24 20:45:26 +0100

    Minor changes backported from #212.

--- manifests/kube-system/cilium/config/cilium-l2-announcement-policy.yaml.nix
+++ manifests/kube-system/cilium/config/cilium-l2-announcement-policy.yaml.nix
@@ -7,6 +7,7 @@ k.api "CiliumL2AnnouncementPolicy.cilium.io" {
   metadata.name = "${cluster.name}-ips-l2-policy";
   spec = {
     loadBalancerIPs = true;
+    # TODO: Exclude nodes with 100Mbps network interfaces.
     nodeSelector.matchLabels."kubernetes.io/arch" = "amd64";
   };
 }
