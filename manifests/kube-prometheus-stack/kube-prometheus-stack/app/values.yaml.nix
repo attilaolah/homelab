b029cc514f91524e7256c0547f35f9f6b159782b
Attila Oláh <attila@dorn.haus>
2025-03-25 22:24:30 +0100

    Configure Grafana OAuth.
    Also Grafana mTLS & Prometheus TLS.

--- manifests/kube-prometheus-stack/kube-prometheus-stack/app/values.yaml.nix
+++ manifests/kube-prometheus-stack/kube-prometheus-stack/app/values.yaml.nix
@@ -3,25 +3,38 @@
   k,
   lib,
   ...
-}: {
-  grafana = let
-    inherit (builtins) attrValues mapAttrs;
-    inherit (cluster) domain;
-    inherit (lib.strings) concatStringsSep;
+}: let
+  inherit (builtins) attrValues mapAttrs;
+  inherit (cluster) domain;
+  inherit (lib.strings) concatStringsSep;
 
+  instance = k.appname ./.;
+  namespace = k.nsname ./.;
+  hosts = [domain];
+  tls = [
+    {
+      inherit hosts;
+      secretName = "${domain}-tls";
+    }
+  ];
+in {
+  grafana = let
     name = "grafana";
     path = "/${name}";
   in {
-    ingress = let
-      hosts = [domain];
-    in {
-      inherit hosts path;
+    ingress = {
+      inherit hosts path tls;
 
       enabled = true;
       ingressClassName = "nginx";
       annotations = {
         # TLS
         "cert-manager.io/cluster-issuer" = "letsencrypt";
+        # Ingress
+        "nginx.ingress.kubernetes.io/backend-protocol" = "HTTPS";
+        "nginx.ingress.kubernetes.io/proxy-ssl-name" = "kube-prometheus-stack-grafana";
+        "nginx.ingress.kubernetes.io/proxy-ssl-secret" = "kube-prometheus-stack/grafana-tls";
+        "nginx.ingress.kubernetes.io/proxy-ssl-verify" = "on";
         # Homepage
         "gethomepage.dev/enabled" = "true";
         "gethomepage.dev/name" = "Grafana";
@@ -30,17 +43,8 @@
         "gethomepage.dev/icon" = "${name}.svg";
         "gethomepage.dev/pod-selector" =
           concatStringsSep ","
-          (attrValues (mapAttrs (key: val: "app.kubernetes.io/${key}=${val}") {
-            inherit name;
-            instance = k.appname ./.;
-          }));
+          (attrValues (mapAttrs (key: val: "app.kubernetes.io/${key}=${val}") {inherit name instance;}));
       };
-      tls = [
-        {
-          inherit hosts;
-          secretName = "${domain}-tls";
-        }
-      ];
     };
 
     admin = let
@@ -52,20 +56,175 @@
     };
 
     # Grafana's primary configuration.
-    "grafana.ini".server = {
-      enforce_domain = true;
-      serve_from_sub_path = true;
-      root_url = "https://${domain}${path}";
+    "grafana.ini" = {
+      server = {
+        enforce_domain = true;
+        serve_from_sub_path = true;
+        root_url = "https://${domain}${path}";
+        protocol = "https";
+        cert_file = "/etc/tls/tls.crt";
+        cert_key = "/etc/tls/tls.key";
+      };
+      "auth.generic_oauth" = {
+        enabled = true;
+        name = "OAuth";
+        scopes = "openid email profile";
+        email_attribute_name = "email";
+        login_attribute_path = "preferred_username";
+        name_attribute_path = "join(' ', [firstName, lastName])";
+        role_attribute_path = "('Admin')"; # everyone is an admin, for now
+        client_id = "monitoring";
+        client_secret = "$__file{/etc/secrets/oauth2_client_secret}";
+        allow_sign_up = true;
+        allowed_domains = hosts;
+        auth_url = "https://dorn.haus/keycloak/realms/dh/protocol/openid-connect/auth";
+        token_url = "https://dorn.haus/keycloak/realms/dh/protocol/openid-connect/token";
+        api_url = "https://dorn.haus/keycloak/realms/dh/protocol/openid-connect/userinfo";
+        use_pkce = true;
+        use_refresh_token = true;
+      };
     };
 
-    additionalDataSources = [
+    prune = true;
+    # Prometheus Datasource installed manually below.
+    sidecar.datasources.defaultDatasourceEnabled = false;
+
+    additionalDataSources = let
+      prometheus = "prometheus";
+      jaeger = "jaeger";
+      loki = "loki";
+
+      secureJsonData = {
+        tlsCACert = "$__file{/etc/tls/ca.crt}";
+        tlsClientCert = "$__file{/etc/tls/tls.crt}";
+        tlsClientKey = "$__file{/etc/tls/tls.key}";
+      };
+      version = 1;
+    in [
+      rec {
+        inherit secureJsonData version;
+        type = prometheus;
+        name = "Prometheus";
+        uid = type;
+        editable = false;
+        url = "https://${instance}-${name}:9090/prometheus";
+        jsonData = {
+          tlsAuth = true;
+          tlsAuthWithCACert = true;
+        };
+      }
+      rec {
+        inherit secureJsonData version;
+        type = jaeger;
+        name = "Jaeger";
+        uid = type;
+        editable = false;
+        url = "https://jaeger-query-https.observability.svc/jaeger";
+        jsonData = {
+          tlsAuth = true;
+          tlsAuthWithCACert = true;
+          tracesToMetrics.datasourceUid = prometheus;
+        };
+      }
       {
+        type = loki;
         name = "Loki";
-        type = "loki";
         url = "http://loki.observability.svc:3100";
+      }
+    ];
+
+    extraSecretMounts = let
+      readOnly = true;
+    in [
+      {
+        inherit readOnly;
+        name = "secrets";
+        mountPath = "/etc/secrets";
+        secretName = name;
+      }
+      {
+        inherit readOnly;
+        name = "tls";
+        mountPath = "/etc/tls";
+        secretName = "${name}-tls";
       }
     ];
   };
 
+  prometheus = let
+    name = "prometheus";
+    path = "/${name}";
+    secretName = "${name}-tls";
+  in rec {
+    prometheusSpec = {
+      retention = "28d";
+      routePrefix = path; # should not be necessary
+      externalUrl = "https://${domain}${path}";
+      additionalArgs = [
+        {name = "web.enable-otlp-receiver";}
+      ];
+      web.tlsConfig = {
+        cert.secret = {
+          name = secretName;
+          key = "tls.crt";
+        };
+        keySecret = {
+          name = secretName;
+          key = "tls.key";
+        };
+        client_ca.secret = {
+          name = secretName;
+          key = "ca.crt";
+        };
+        # NOTE: RequireAndVerifyClientCert causes startup probes to fail.
+        clientAuthType = "VerifyClientCertIfGiven";
+      };
+
+      storageSpec.emptyDir.medium = "Memory";
+
+      volumes = [
+        {
+          name = "tls";
+          secret = {inherit secretName;};
+        }
+      ];
+
+      volumeMounts = [
+        {
+          name = "tls";
+          mountPath = "/etc/tls";
+          readOnly = true;
+        }
+      ];
+    };
+
+    # containers: oauth-proxy!
+    serviceMonitor = {
+      scheme = "https";
+      tlsConfig = {
+        inherit (prometheusSpec.web.tlsConfig) cert keySecret client_ca;
+        serverName = "${instance}-${name}";
+      };
+    };
+  };
+
   cleanPrometheusOperatorObjectNames = true;
+
+  extraManifests = map (name:
+    k.api "Certificate.cert-manager.io" {
+      metadata = {inherit name;};
+      spec = {
+        secretName = "${name}-tls";
+        issuerRef = {
+          kind = "ClusterIssuer";
+          name = "internal-ca";
+        };
+        commonName = name;
+        dnsNames = ["${namespace}-${name}"];
+      };
+    }) [
+    "grafana"
+    "prometheus"
+    "alertmanager"
+  ];
 }
