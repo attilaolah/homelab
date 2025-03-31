f172aae47f24343ba8a434e7a08e7004b5eea2b9
Attila Oláh <attila@dorn.haus>
2025-04-01 00:22:45 +0200

    Generate JSON response from Nix.

--- manifests/default/well-known/app/config-map.yaml.nix
+++ manifests/default/well-known/app/config-map.yaml.nix
@@ -6,15 +6,30 @@
 }:
 k.api "ConfigMap" (let
   inherit (cluster) domain;
+  inherit (lib.strings) escape toJSON;
 
   name = k.appname ./.;
   labels = import ./labels.nix;
 in {
   metadata = {inherit name labels;};
   data = {
-    config = ''
+    config = let
+      ok = toJSON {
+        subject = "acct:$account@${domain}";
+        links = [
+          {
+            "rel" = "http://openid.net/specs/connect/1.0/issuer";
+            "href" = "https://${domain}/keycloak/realms/dh";
+          }
+        ];
+      };
+      notFound = toJSON {
+        code = 404;
+        error = "resource not found";
+      };
+    in ''
       map $query_string $account {
-          ~resource=acct(%3[Aa]|:)(?P<user>.+?)(%40|@)${lib.strings.escape ["."] domain}$ $user;
+          ~resource=acct(%3[Aa]|:)(?P<user>.+?)(%40|@)${escape ["."] domain}$ $user;
       }
 
       server {
@@ -22,19 +37,19 @@ in {
           listen [::]:8443 ssl;
           server_name ${domain};
 
-          ssl_certificate /etc/tls/tls.crt;
-          ssl_certificate_key /etc/tls/tls.key;
-          ssl_client_certificate /etc/tls/ca.crt;
+          ssl_certificate ${k.pki.crt};
+          ssl_certificate_key ${k.pki.key};
+          ssl_client_certificate ${k.pki.ca};
           ssl_verify_client on;
 
           location /.well-known/webfinger {
               default_type application/json;
 
               if ($account) {
-                  return 200 '{"subject":"acct:$account@${domain}","links":[{"rel":"http://openid.net/specs/connect/1.0/issuer","href":"https://${domain}/keycloak/realms/dh"}]}';
+                  return 200 '${escape ["'"] ok}';
               }
 
-              return 404 '{"code":404,"error":"resource not found"}';
+              return 404 '${notFound}';
           }
       }
     '';
