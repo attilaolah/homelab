8cda50a651e79c8f60fd3adabcbd5550143e7c39
Attila Oláh <attila@dorn.haus>
2025-04-01 14:07:33 +0200

    Disable cost estimation suggestion.

--- manifests/default/goldilocks/app/values.yaml.nix
+++ manifests/default/goldilocks/app/values.yaml.nix
@@ -1,4 +1,12 @@
-{v, ...}: {
+# https://artifacthub.io/packages/helm/fairwinds-stable/goldilocks#values
+# https://github.com/FairwindsOps/charts/blob/master/stable/goldilocks/values.yaml
+{
+  k,
+  v,
+  ...
+}: let
+  name = k.appname ./.;
+in {
   image.tag = v.goldilocks.docker;
   controller.resources = rec {
     limits = requests // {cpu = "200m";};
@@ -8,12 +16,16 @@
       ephemeral-storage = "256Mi";
     };
   };
-  dashboard.resources = rec {
-    limits = requests // {cpu = "100m";};
-    requests = {
-      cpu = "50m";
-      memory = "128Mi";
-      ephemeral-storage = "128Mi";
+  dashboard = {
+    basePath = "/${name}";
+    flags.enable-cost = "false";
+    resources = rec {
+      limits = requests // {cpu = "100m";};
+      requests = {
+        cpu = "50m";
+        memory = "128Mi";
+        ephemeral-storage = "128Mi";
+      };
     };
   };
 }
