a70b6d978bc81467a5fbea2dbfc0b62f9f347f53
Attila Oláh <attila@dorn.haus>
2025-08-02 01:40:57 +0200

    feat: helm template checks (#414)
    * feat: separate oci and yaml packages
    
    * fix: nix run #deploy should use the yaml artifacts
    
    * feat: add #expand app to generate helm manifests
    
    * feat: store helm generated manifests in helm directory
    
    * feat: run qa steps in a separate workflow

--- modules/manifests/default.nix
+++ modules/manifests/default.nix
@@ -58,34 +58,34 @@
 
     # OCI tar archive containing all manifests, used as build output.
     manifests-oci = pkgs.stdenv.mkDerivation (finalAttrs: {
-      pname = "manifests";
-      version = "latest";
+      name = "manifests.tar.gz";
 
       src = manifests-dir;
       phases = ["installPhase"];
       nativeBuildInputs = with pkgs; [fluxcd];
       installPhase = ''
-        temp_tgz="$(mktemp -d)/manifests.tgz"
-        flux build artifact --path="$src" --output="$temp_tgz"
-        mkdir --parents "$out"
-        tar --directory="$out" --file="$temp_tgz" --extract --gzip
+        flux build artifact --path="$src" --output="$out"
       '';
+    });
 
-      meta = let
-        inherit (pkgs) lib;
-      in {
-        description = "Kubernetes Manifests";
-        homepage = with cluster.github; "https://github.com/${owner}/${repository}";
-        license = with lib.licenses; [asl20 mit];
-        maintainers = with lib.maintainers; [attila];
-      };
+    # FluxCD YAML manifests, extracted from the OCI artifact.
+    manifests-yaml = pkgs.stdenv.mkDerivation (finalAttrs: {
+      name = "manifests-yaml";
+
+      src = manifests-oci;
+      phases = ["installPhase"];
+      nativeBuildInputs = with pkgs; [gnutar];
+      installPhase = ''
+        mkdir --parents "$out"
+        tar --directory="$out" --file="$src" --extract --gzip
+      '';
     });
   in {
     devenv.shells.default.env.MANIFESTS = manifests-dir;
 
     packages = {
-      inherit manifests-oci;
-      default = manifests-oci;
+      inherit manifests-oci manifests-yaml;
+      default = manifests-yaml;
     };
 
     apps = let
@@ -98,7 +98,7 @@
           runtimeInputs = with pkgs; [coreutils fluxcd git];
           text = ''
             TEMP_DIR="$(mktemp --directory)"
-            cp --recursive "${manifests-oci}"/* "$TEMP_DIR"
+            cp --recursive "${manifests-yaml}"/* "$TEMP_DIR"
             chmod --recursive +w "$TEMP_DIR"
             flux push artifact "${artifactURI}" \
               --path="$TEMP_DIR" \
@@ -112,6 +112,88 @@
     in {
       inherit deploy;
       default = deploy;
+
+      expand = {
+        type = "app";
+        program = pkgs.writeShellApplication {
+          name = "deploy";
+          runtimeInputs = with pkgs; [coreutils yq];
+          text = ''
+            find "${manifests-yaml}" -name helm-release.yaml -print0 |
+              while IFS= read -r -d "" helm_release
+              do
+              repo_kind="$(yq < "$helm_release" --raw-output .spec.chart.spec.sourceRef.kind)"
+              if [[ "$repo_kind" == "GitRepository" ]]; then
+                # TODO: Support Git Helm repositories.
+                continue
+              fi
+
+              app_dir="$(dirname "$helm_release")"
+              release_dir="$(dirname "$app_dir")"
+              namespace_dir="$(dirname "$release_dir")"
+              namespace="$(basename "$namespace_dir")"
+
+              # Sanity check: make sure a namespace manifest is generated.
+              namespace_name="$(yq < "$namespace_dir/namespace.yaml" --raw-output .metadata.name)"
+              if [[ "$namespace_name" != "$namespace" ]]; then
+                echo >&2 "ERROR: while processing release $helm_release:"
+                echo >&2 "ERROR: unexpected namespace: $namespace_name != $namespace"
+                exit 1
+              fi
+
+              # Sanity check: make sure the release name matches directory name.
+              release_name="$(basename "$release_dir")"
+              release="$(yq < "$helm_release" --raw-output .metadata.name)"
+              if [[ "$release_name" != "$release" ]]; then
+                echo >&2 "ERROR: while processing release $helm_release:"
+                echo >&2 "ERROR: unexpected release: $release_name != $release"
+                exit 1
+              fi
+
+              repo_url="$(
+                cat "${manifests-yaml}/flux-system"/*-repository.yaml |
+                yq --raw-output '
+                  select(.kind=='"$(yq < "$helm_release" .spec.chart.spec.sourceRef.kind)"') |
+                  select(.metadata.namespace=='"$(yq < "$helm_release" .spec.chart.spec.sourceRef.namespace)"') |
+                  select(.metadata.name=='"$(yq < "$helm_release" .spec.chart.spec.sourceRef.name)"') |
+                  .spec.url
+                '
+              )"
+              chart="$(yq < "$helm_release" --raw-output .spec.chart.spec.chart)"
+              version="$(yq < "$helm_release" --raw-output .spec.chart.spec.version)"
+
+              helm_flags=(
+                "--namespace=$namespace"
+                "--version=$version"
+              )
+              if [[ "$repo_url" == "oci://"* ]]; then
+                chart="$repo_url/$chart"
+              else
+                helm_flags+=("--repo=$repo_url")
+              fi
+
+              while IFS= read -r config_map; do
+                while IFS= read -r values_yaml; do
+                  helm_flags+=("--values=$app_dir/$values_yaml")
+                done < <(yq < "$app_dir/kustomization.yaml" --raw-output '
+                  .configMapGenerator[] |
+                  select(.name=='"$config_map"') |
+                  .files[]
+                ')
+              done < <(yq < "$helm_release" '
+                .spec.valuesFrom[] |
+                select(.kind=="ConfigMap") |
+                .name
+              ')
+
+              out_dir="helm/$namespace"
+              mkdir --parents "$out_dir"
+              echo "Running: helm template $release $chart ..."
+              helm template "$release" "$chart" "''${helm_flags[@]}" > "$out_dir/$release.yaml"
+            done
+          '';
+        };
+      };
     };
   };
 }
