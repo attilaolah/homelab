cbbf2044f5e9e509d1ee024f9f090e15d11ed73e
Attila Oláh <attila@dorn.haus>
2025-08-02 04:01:48 +0200

    fix: apply inadyn patches

--- manifests/infrastructure/inadyn/app/helm-release.yaml.nix
+++ manifests/infrastructure/inadyn/app/helm-release.yaml.nix
@@ -1,1 +1,30 @@
-{k, ...}: k.fluxcd.helm-release ./. {}
+{
+  k,
+  self,
+  ...
+}:
+k.fluxcd.helm-release ./. {
+  spec.postRenderers = [
+    {
+      kustomize.patches = [
+        {
+          target = {
+            kind = "Deployment";
+            name = k.appname ./.;
+          };
+          patch = self.lib.yaml.format [
+            {
+              op = "add";
+              path = "/spec/template/spec/automountServiceAccountToken";
+              value = false;
+            }
+            {
+              op = "remove";
+              path = "/spec/template/spec/serviceAccountName";
+            }
+          ];
+        }
+      ];
+    }
+  ];
+}
