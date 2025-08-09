f444e92005655a7d7cfadb67084576e1d96497cd
Attila Oláh <attila@dorn.haus>
2025-08-09 19:35:43 +0200

    fix: hide x-powered-by header

--- manifests/ingress-nginx/ingress-nginx/app/values.yaml.nix
+++ manifests/ingress-nginx/ingress-nginx/app/values.yaml.nix
@@ -1,3 +1,5 @@
+# https://artifacthub.io/packages/helm/ingress-nginx/ingress-nginx#values
+# https://github.com/kubernetes/ingress-nginx/blob/main/charts/ingress-nginx/values.yaml
 inputs @ {cluster, ...}: let
   namespace = "ingress-nginx";
   certificate = import ../config/certificate.yaml.nix inputs;
@@ -7,5 +9,9 @@ in {
     ingressClassResource.default = true;
     service.annotations."lbipam.cilium.io/ips" = cluster.network.external.ingress;
     extraArgs.default-ssl-certificate = "${namespace}/${certificate.spec.secretName}";
+
+    # Add & remove headers:
+    addHeaders."Content-Security-Policy" = "frame-ancestors 'self'";
+    config.hide-headers = "X-Powered-By";
   };
 }
