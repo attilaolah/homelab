d2d8a24819cb01b26756b0e0d8633190f29705f4
Attila Oláh <attila@dorn.haus>
2025-08-09 21:14:10 +0200

    feat: ingress-nginx add + remove headers (#460)
    * fix: hide x-powered-by header
    
    * fix: also hide x-nextjs-cache
    
    * fix: remove deprecated x-xss-protection, add x-content-type-options
    
    * cleanup: remove comment
    
    * feat: more restrictive csp
    
    * fix: relax content-security policy

--- manifests/ingress-nginx/ingress-nginx/app/values.yaml.nix
+++ manifests/ingress-nginx/ingress-nginx/app/values.yaml.nix
@@ -1,4 +1,11 @@
-inputs @ {cluster, ...}: let
+# https://artifacthub.io/packages/helm/ingress-nginx/ingress-nginx#values
+# https://github.com/kubernetes/ingress-nginx/blob/main/charts/ingress-nginx/values.yaml
+inputs @ {
+  cluster,
+  lib,
+  ...
+}: let
+  inherit (lib.strings) concatStringsSep;
   namespace = "ingress-nginx";
   certificate = import ../config/certificate.yaml.nix inputs;
 in {
@@ -7,5 +14,26 @@ in {
     ingressClassResource.default = true;
     service.annotations."lbipam.cilium.io/ips" = cluster.network.external.ingress;
     extraArgs.default-ssl-certificate = "${namespace}/${certificate.spec.secretName}";
+
+    addHeaders = {
+      "content-security-policy" = concatStringsSep "; " [
+        "default-src 'self'"
+        # Homepage service icons are served from jsdelivr.
+        "img-src 'self' https://cdn.jsdelivr.net"
+        "script-src 'self' 'unsafe-inline'"
+        "style-src 'self' 'unsafe-inline'"
+        "form-action 'self'"
+        "frame-ancestors 'self'"
+        "object-src 'none'"
+      ];
+      "x-content-type-options" = "nosniff";
+    };
+    config.hide-headers = concatStringsSep "," [
+      # NextJS (Homepage) headers:
+      "x-nextjs-cache"
+      "x-powered-by"
+      # Old XSS protection headers:
+      "x-xss-protection"
+    ];
   };
 }
