354fe6c96533fe7a6d27fd3c4da4fb9951fdf5b0
Attila Oláh <attila@dorn.haus>
2025-08-23 22:20:09 +0200

    fix: keycloak certificate & probe delay (#499)

--- manifests/keycloak/keycloak/ks.yaml.nix
+++ manifests/keycloak/keycloak/ks.yaml.nix
@@ -1,6 +1,9 @@
 {k, ...}:
 k.fluxcd.kustomization ./. {
-  app.spec.dependsOn = map k.fluxcd.dep [./database];
+  app.spec = {
+    dependsOn = map k.fluxcd.dep [./database];
+    timeout = "20m";
+  };
   database.spec.dependsOn = map k.fluxcd.dep [
     ../../cnpg-system/cloudnative-pg/app
   ];
