6b0358e759612eee4f2e7de72521d8665fbb6846
Attila Oláh <attila@dorn.haus>
2025-11-18 08:16:06 +0100

    chore(deps): update talosctl hashes

--- modules/talosctl.nix
+++ modules/talosctl.nix
@@ -6,7 +6,7 @@ in {
   inherit version;
   src = fetchTarball {
     url = "https://github.com/siderolabs/talos/archive/refs/tags/v${version}.tar.gz";
-    sha256 = "0snz8hs92lx858w0iwn6plc46p1n85wbwrcrr0fpaf0d04mb8yga";
+    sha256 = "sha256:1956zvmra93y6vp531iyg274mvlm2nkpqcyncd2lh5gy1vbrjxg7";
   };
-  vendorHash = "sha256-NLyWzkagiP6zeeB4o6CI9UBPH6a5JGhPu1QGyiovBfM=";
+  vendorHash = "sha256-ocU7vpSdUdVzOFcqa+QWRdcP9SnC6WtV/ruheSGUfg4=";
 }))
