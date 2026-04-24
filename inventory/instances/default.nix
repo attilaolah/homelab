4ecd58d1b1829ac9bf1531fb3f0b12e1dc6d2ea3
Attila Oláh <attila@dorn.haus>
2026-04-24 08:51:03 +0200

    refactor: dynamically import tag modules

--- inventory/instances/default.nix
+++ inventory/instances/default.nix
@@ -6,17 +6,16 @@
 
   # Additional module imports by tag.
   # https://docs.clan.lol/latest/services/definition/
-  inventory.instances = builtins.listToAttrs (map (tag: {
-      name = "settings-${tag}";
-      value = {
-        module.name = "importer";
-        roles.default = {
-          tags.${tag} = {};
-          extraModules = [../../modules/tags/${tag}.nix];
-        };
+  inventory.instances = builtins.listToAttrs (map (module: let
+    tag = builtins.replaceStrings [".nix"] [""] module;
+  in {
+    name = "settings-${tag}";
+    value = {
+      module.name = "importer";
+      roles.default = {
+        tags.${tag} = {};
+        extraModules = [../../modules/tags/${module}];
       };
-    }) [
-      "all"
-      "laptop"
-    ]);
+    };
+  }) (builtins.attrNames (builtins.readDir ../../modules/tags)));
 }
