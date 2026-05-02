83a926ceba0bb1eb1f9639a61019780dd4d72ece
Attila Oláh <attila@dorn.haus>
2026-05-02 22:43:19 +0200

    cleanup: remove unused systems

--- flake.nix
+++ flake.nix
@@ -34,9 +34,7 @@
       nixpkgs.lib.genAttrs
       [
         "x86_64-linux"
-        "aarch64-linux"
         "aarch64-darwin"
-        "x86_64-darwin"
       ]
       (system: {
         default = clan-core.inputs.nixpkgs.legacyPackages.${system}.mkShell {
