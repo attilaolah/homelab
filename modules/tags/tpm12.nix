37856d796eb517b2252b5a056a30238a049c27d0
Attila Oláh <attila@dorn.haus>
2026-05-05 20:18:25 +0200

    feat: add tpm owner password generator

--- modules/tags/tpm12.nix
+++ modules/tags/tpm12.nix
@@ -1,7 +1,18 @@
-{
+{pkgs, ...}: {
+  clan.core.vars.generators.tpm-owner-auth = {
+    files."owner-auth" = {
+      secret = true;
+      deploy = false;
+    };
+    runtimeInputs = with pkgs; [xkcdpass];
+    script = ''
+      xkcdpass --numwords 6 --delimiter - --count 1 | tr -d "\n" > "$out/owner-auth"
+    '';
+  };
+
   services.tcsd.enable = true;
 
-  # trousers/tcsd runs as user/group tss and needs access to TPM device nodes.
+  # Trousers/tcsd runs as user/group tss and needs access to TPM device nodes.
   services.udev.extraRules = ''
     SUBSYSTEM=="tpm", KERNEL=="tpm[0-9]*", GROUP="tss", MODE="0660"
     SUBSYSTEM=="tpmrm", KERNEL=="tpmrm[0-9]*", GROUP="tss", MODE="0660"
