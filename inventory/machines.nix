13898a7e40129a87b6480d9981fee18b7fdea4f8
Attila Oláh <attila@dorn.haus>
2026-05-07 22:31:55 +0200

    fix(pki): restrict ACME clients to non-TPM hosts

--- inventory/machines.nix
+++ inventory/machines.nix
@@ -4,11 +4,14 @@
     inherit (data) ids machines tags;
 
     data = import ./data.nix;
+    tpmMachines = tags.tpm12 ++ (tags.tpm12_bootstrap or []);
   in
     # Make sure no tags contains a machine that is not registered.
     assert (filter (machine: !(hasAttr machine ids)) (concatLists (attrValues tags))) == [];
     # Make sure ACME servers only run on machines with TPM 1.2 hardware.
     assert all (machine: elem machine tags.tpm12) tags.acme;
+    # Make sure ACME clients only run on machines without TPM hardware.
+    assert all (machine: !(elem machine tpmMachines)) tags.acme_client;
       mapAttrs (_name: machine: {
         deploy.targetHost = "root@${machine.ip}";
         tags = machine.tags;
