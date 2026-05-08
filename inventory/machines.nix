bb52e74ed4dfc2045aec07748b244771ba63dc74
Attila Oláh <attila@dorn.haus>
2026-05-08 13:37:43 +0200

    fix(inventory): treat tag assertions as optional

--- inventory/machines.nix
+++ inventory/machines.nix
@@ -4,13 +4,17 @@
     inherit (data) ids machines tags;
 
     data = import ./data.nix;
+    acme = tags.acme or [];
+    acme_client = tags.acme_client or [];
+    tpm12 = tags.tpm12 or [];
+    tpm12_bootstrap = tags.tpm12_bootstrap or [];
   in
     # Make sure no tags contains a machine that is not registered.
     assert (filter (machine: !(hasAttr machine ids)) (concatLists (attrValues tags))) == [];
     # Make sure ACME servers only run on machines with TPM 1.2 hardware.
-    assert all (machine: elem machine tags.tpm12) tags.acme;
+    assert all (machine: elem machine tpm12) acme;
     # Make sure ACME clients only run on machines without TPM hardware.
-    assert all (machine: !(elem machine (tags.tpm12 ++ (tags.tpm12_bootstrap or [])))) tags.acme_client;
+    assert all (machine: !(elem machine (tpm12 ++ tpm12_bootstrap))) acme_client;
       mapAttrs (_name: machine: {
         deploy.targetHost = "root@${machine.ip}";
         tags = machine.tags;
